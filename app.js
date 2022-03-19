af8a659e3fafa05aa64d4e18f353136c41025072
Unlilt <jade.t.tavai@gmail.com>
2022-03-19 15:36:17 +1300

    target data
    - Added bg to sections
    - Set js to target corresponding section id when Btn clicked

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 
 function PageTransitions() {
+    //change activeBtn
     for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.activeBtn');
@@ -11,6 +12,11 @@ function PageTransitions() {
             this.className += ' activeBtn';
         })
     }
+
+    //section active class
+    allSections.addEventListener('click', function(e){
+        const id = e.target.dataset.id;
+    })
 }
 
 PageTransitions();
